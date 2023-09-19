59078f902370d7daf6f8faadbf5217b1508529c3
zahiddkhan <zahid.innovatorx@gmail.com>
2023-09-19 14:41:46 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import logo from "/images/media.png?ur"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='main'>
+       <div className='main-content'>
+       <h2>Technology</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, sint ducimus quas est ab maxime accusantium nulla ipsum voluptatum? Optio nemo enim doloremque labore eligendi esse veniam incidunt, ipsa eum?
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, sint ducimus quas est ab maxime accusantium nulla ipsum voluptatum? Optio nemo enim doloremque labore eligendi esse veniam incidunt, ipsa eum?
+    </p>
+       </div>
+
+       <div className="mian-image">
+        <img src={logo} alt="img" />
+       </div>
+    </div>
+       
+    </>
+  )
+}
+
+export default App
